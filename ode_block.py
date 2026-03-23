f6958de2174a77a17811264c281906f88d1249c0
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-03-23 19:12:25 +0530

    Create ode_block.py

---
+++ ode_block.py
@@ -0,0 +1,11 @@
+from torchdiffeq import odeint
+
+class ODEEvolution(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x):
+        t = torch.tensor([0, 1]).float().to(x.device)
+        out = odeint(self.func, x, t, method='rk4')  # or 'dopri5'
+        return out[-1]
