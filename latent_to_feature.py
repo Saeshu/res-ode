129dfb81f08d171c9b9d27c2d7f47a4532cbfdb9
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-03-23 19:10:52 +0530

    Implement LatentToFeature class for feature extraction

---
+++ latent_to_feature.py
@@ -0,0 +1,8 @@
+class LatentToFeature(nn.Module):
+    def __init__(self, z_dim=128, base_ch=128):
+        super().__init__()
+        self.fc = nn.Linear(z_dim, base_ch * 4 * 4)
+
+    def forward(self, z):
+        x = self.fc(z)
+        return x.view(z.size(0), -1, 4, 4)  # (B, C, 4, 4)
