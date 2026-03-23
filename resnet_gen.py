1fed78f8c224583e442e5122ad3e4880d9284ec1
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-03-23 19:13:33 +0530

    Create resnet_gen.py

---
+++ resnet_gen.py
@@ -0,0 +1,11 @@
+class ResNetGenerator(nn.Module):
+    def __init__(self, z_dim=128, ch=128):
+        super().__init__()
+        self.init = LatentToFeature(z_dim, ch)
+        self.evolve = ResNetEvolution(ch)
+        self.to_img = ToImage(ch)
+
+    def forward(self, z):
+        x = self.init(z)
+        x = self.evolve(x)
+        return self.to_img(x)
