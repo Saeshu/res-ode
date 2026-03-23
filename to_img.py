860c507568d4f60658932e9effabd7872647dbb6
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-03-23 19:13:07 +0530

    Create to_img.py

---
+++ to_img.py
@@ -0,0 +1,14 @@
+class ToImage(nn.Module):
+    def __init__(self, base_ch=128):
+        super().__init__()
+        self.net = nn.Sequential(
+            UpsampleBlock(base_ch, 64),   # 4 → 8
+            UpsampleBlock(64, 32),       # 8 → 16
+            UpsampleBlock(32, 16),       # 16 → 32
+            nn.Conv2d(16, 3, 3, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+      
